24f2244633a88ca9d89f9ca3d42476deb9c96b11
Daniel Rebelsky <drebelsky@gmail.com>
2018-07-21 14:02:57 -0500

    Made it so rhyming words are unique

--- Final Examples/Rhyme/server.rkt
+++ Final Examples/Rhyme/server.rkt
@@ -57,22 +57,26 @@
 (define (one-of-list list)
   (list-ref list (random (length list))))
 
-(define (GenerateSentence rhymes)
-  (define str (string-append (noun) " " (transitive-verb) " " (one-of "a" "the") " " (one-of-list rhymes) "."))
+(define (GenerateSentence end)
+  (define str (string-append (noun) " " (transitive-verb) " " (one-of "a" "the") " " end "."))
   (string-append (string-upcase (substring str 0 1)) (substring str 1)))
 
 (define (GenerateRhyme request)
   (define ac (one-of-list rhymes))
-  (define bd (one-of-list rhymes))
+  (define bd (one-of-list (remove ac rhymes)))
+  (define noun1 (one-of-list ac))
+  (define noun2 (one-of-list bd))
+  (define noun3 (one-of-list (remove noun1 ac)))
+  (define noun4 (one-of-list (remove noun2 bd)))
   `(*TOP*
     (html
      (head
       )
      (body
-      (p ,(GenerateSentence ac))
-      (p ,(GenerateSentence bd))
-      (p ,(GenerateSentence ac))
-      (p ,(GenerateSentence bd))
+      (p ,(GenerateSentence noun1))
+      (p ,(GenerateSentence noun2))
+      (p ,(GenerateSentence noun3))
+      (p ,(GenerateSentence noun4))
       ))))
 
 (serve-procedure "" GenerateRhyme)
